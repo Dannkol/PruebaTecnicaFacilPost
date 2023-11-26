3e0bd5909a6b4b5bfcda5ac98c7fea2a3a8bbbc7
Daniel Manosalva <manosalvasierra@gmail.com>
2023-11-26 17:35:17 -0500

    style: :lipstick: Index mas amigable al usuario
    Se creo un index mas amigable y milimalista
    
    @dannkol

--- app/Index/__init__.py
+++ app/Index/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
 
-Index = Blueprint('Index', __name__, template_folder='templates', static_folder='static')
+Index = Blueprint('Index', __name__, template_folder='templates', static_folder='static', static_url_path='/Index')
 
 from . import routes
